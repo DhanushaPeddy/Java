f0d1726c9ac1e6f83ffbc8e677a2770270984d70
DhanushaPeddy <52650112+DhanushaPeddy@users.noreply.github.com>
2020-08-25 12:37:16 +0530

    Create TestInheritance.java

---
+++ CoreJava/Inheritance/TestInheritance.java
@@ -0,0 +1,11 @@
+
+public class TestInheritance {
+	public static void main(String[] args) {
+//		WageEmployee w = new WageEmployee();
+//		w.display();
+//		WageEmployee w1 = new WageEmployee(404,"Jeeth",250, 11);
+//		w1.display();
+		SalariedEmployee s = new SalariedEmployee(404,"Dhanu",45000,4000,2000);
+		s.display();
+	}
+}
