824ccf3ab9c31b89a91993ae2b15ab74af41c7b9
DhanushaPeddy <52650112+DhanushaPeddy@users.noreply.github.com>
2020-08-26 09:28:02 +0530

    Create TestInterface.java

---
+++ CoreJava/ShapeInterfaceAssignement/TestInterface.java
@@ -0,0 +1,10 @@
+
+public class TestInterface {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Shape shape=new Circle(10);		
+		Shape shape2=new Square(4);
+		Shape shape3=new Rectangle(4,2);
+	}
+}
