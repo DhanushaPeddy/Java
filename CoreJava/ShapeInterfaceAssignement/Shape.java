55c822bdbe14f75fbdfea0f321c51c6745eea980
DhanushaPeddy <52650112+DhanushaPeddy@users.noreply.github.com>
2020-08-26 09:30:21 +0530

    Create Shape.java

---
+++ CoreJava/ShapeInterfaceAssignement/Shape.java
@@ -0,0 +1,23 @@
+
+public class Shape {
+	private String name;
+
+	public Shape() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Shape(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
