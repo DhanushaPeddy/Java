02555e992e215064546eb41a298925faaf310bc8
DhanushaPeddy <52650112+DhanushaPeddy@users.noreply.github.com>
2020-08-26 09:31:21 +0530

    Update Circle.java

--- CoreJava/ShapeInterfaceAssignement/Circle.java
+++ CoreJava/ShapeInterfaceAssignement/Circle.java
@@ -10,8 +10,8 @@ public class Circle extends Shape implements Calculatable {
 
 	public void display() {
 		// TODO Auto-generated method stub
-		System.out.println("Area of circle : "+area());
-		System.out.println("Perimeter of circle : "+perimeter());
+		System.out.println("Area of " +getName()+" : "+area());
+		System.out.println("Perimeter of " +getName()+" : "+perimeter());
 	}
 	public double area() {
 		return 3.14*radius*radius;
